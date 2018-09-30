75c25f250e263ef86a12e986940475af8c7a8ec4
kawa1228 <sxa.1105bo@gmail.com>
2018-09-30 16:15:55 +0900

    add gitignore

--- src/scripts/entries/App.jsx
+++ src/scripts/entries/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import { apiKey } from '../../myApiKey'
 
 class App extends React.Component {
   constructor() {
@@ -12,13 +13,13 @@ class App extends React.Component {
   }
   componentWillMount() {
     axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures',
-      { headers: { 'X-API-KEY': 'MY-API-KEY' } })
+      { headers: { 'X-API-KEY': apiKey } })
       .then(res => console.log(res.data.result))
   }
   getRegionalData(index) {
     console.log('hello')
     axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/sum/perYear?cityCode=-&prefCode=${index}`,
-      { headers: { 'X-API-KEY': 'MY-API-KEY' } })
+      { headers: { 'X-API-KEY': apiKey } })
       .then(res => console.log(res.data.result.line.data))
   }
   render() {
