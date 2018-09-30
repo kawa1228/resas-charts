dff120336e9bee4d576b0c4a823e7416026b5023
kawa1228 <sxa.1105bo@gmail.com>
2018-09-30 17:03:59 +0900

    setstate prefectures

--- src/scripts/entries/App.jsx
+++ src/scripts/entries/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import axios from 'axios'
 import { apiKey } from '../../myApiKey'
+// import heighcharts from 'highcharts-react'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
+      prefectures: {},
       tasks: [],
       flag: false
     }
@@ -14,18 +16,28 @@ class App extends React.Component {
   componentWillMount() {
     axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures',
       { headers: { 'X-API-KEY': apiKey } })
-      .then(res => console.log(res.data.result))
+      .then(res => {
+        this.setState({
+          prefectures: res.data.result
+        })
+      })
   }
   getRegionalData(index) {
-    console.log('hello')
     axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/sum/perYear?cityCode=-&prefCode=${index}`,
       { headers: { 'X-API-KEY': apiKey } })
-      .then(res => console.log(res.data.result.line.data))
+      .then(res =>
+        console.log(res.data.result.line.data)
+      )
   }
   render() {
+    console.log(this.state.prefectures[0])
     return (
-      < form >
-      </form >
+      <div>
+        <input
+          type="checkbox"
+        />
+        {/* {this.state.prefecture[0].prefName} */}
+      </div>
     )
   }
 }
