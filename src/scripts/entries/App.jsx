0384713369f13348a8505e76d92537edc641b3b4
kawa1228 <sxa.1105bo@gmail.com>
2018-10-03 11:48:09 +0900

    setSelect

--- src/scripts/entries/App.jsx
+++ src/scripts/entries/App.jsx
@@ -9,7 +9,6 @@ class App extends React.Component {
     super()
     this.state = {
       prefectures: [],
-      population: [],
       select: [],
       flag: Array(47).fill(false)
     }
@@ -27,44 +26,17 @@ class App extends React.Component {
     return axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/sum/perYear?cityCode=-&prefCode=${index}`,
       { headers: { 'X-API-KEY': apiKey } })
       .then(res => {
-        let populations = []
-        res.data.result.line.data.map(val => {
-          populations.push(val.value)
-        })
-        return populations
-        // this.setState({
-        //   population: populations
-        // })
+        return res.data
       })
   }
   handleClick(e) {
-    let index = this.state.prefectures[e.target.value - 1].prefCode
+    let index = e.target.value
 
     this.state.flag[index - 1] = !this.state.flag[index - 1]
     this.setState({
       flag: this.state.flag
     })
-    this.selectSeries()
-  }
-  selectSeries() {
-    let obj = {}
-    for (let i = 0; i < this.state.flag.length; i++) {
-      if (this.state.flag[i]) {
-        obj = {
-          id: i + 1,
-          name: this.state.prefectures[i].prefName
-        }
-      }
-    }
-    this.getRegionalData(obj.id).then((res) => {
-      console.log(res)
-      console.log(obj)
-      obj.data = res
-    })
-    console.log(obj)
-    // this.setState({
-    //   select: obj
-    // })
+    this.setSelect(index)
   }
   renderItems() {
     return this.state.prefectures.map(val => {
@@ -86,7 +58,7 @@ class App extends React.Component {
         text: '都道府県別人口構成'
       },
       series: [{
-        data: this.state.population,
+        data: this.state.select,
         pointStart: 1965,
       }]
     }
@@ -100,7 +72,22 @@ class App extends React.Component {
 
     return App()
   }
+  setSelect(num) {
+    this.getRegionalData(num).then(res => {
+
+      let obj = {}
+      obj = {
+        id: num,
+        name: this.state.prefectures[num - 1].prefName,
+        data: res.result.line.data
+      }
+      this.setState({
+        select: this.state.select.concat(obj)
+      })
+    })
+  }
   render() {
+    console.log(this.state)
     return (
       <div>
         <p>
